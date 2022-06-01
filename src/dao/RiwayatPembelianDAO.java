df8da2ac7f19756f6ac693f6d68cfccad4ac3e29
dumbgoat <dumbgoat.00@gmail.com>
2022-06-01 10:23:09 +0700

    RiwayaatPembelianDAO created uwu

---
+++ src/dao/RiwayatPembelianDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package dao;
+import connection.DbConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RiwayatPembelianDAO {
+    private DbConnection dbCon = new DbConnection();
+    private Connection con;
+    
+}
