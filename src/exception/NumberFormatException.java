7d58c16876cfd565c362ae25d6e7946abcce81ee
dumbgoat <dumbgoat.00@gmail.com>
2022-06-02 16:09:41 +0700

    tambah kelas Exception Inputan Kosong & NumberFormat

--- src/exception/NumberFormatException.java
+++ src/exception/NumberFormatException.java
@@ -2,14 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package exception;
 
 /**
- *
- * @author Teprutz tes
- */ 
-public class InputanKosongException {
-    public String message(){
-        return "Field input tidak boleh kosong!";
+ Nama : Rakai Anandhi Mahardhika
+ NPM : 200710914
+ */
+public class NumberFormatException {
+     public String message(){
+        return "Invalid input!";
     }
 }
